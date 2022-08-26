68d515595868f565ad370a92d7b9115c81851e17
asiaziola <ziola.jm@gmail.com>
2022-08-26 14:49:28 +0200

    adding gitignore

--- unpkg/ts/src/index.d.ts
+++ unpkg/ts/src/index.d.ts
@@ -1,1 +1,5 @@
-export {};
+declare const SOURCE_TX_ID = "9vYCJs70vyrjgXudb6lhHijXelcOd4MV5DsACgmAdoU";
+declare const warp: any;
+declare const sdk: any;
+declare const deployWriteAndRead: () => Promise<any>;
+declare const loadWallet: () => Promise<any>;
