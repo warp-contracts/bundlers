2d2d12a34cea33fa347d0abd49ee798905a0a2a2
ppe <peter@redstone.finance>
2022-08-26 15:03:00 +0200

    feat: update script

---
+++ update-warp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+version="1.2.0-bundles.16"
+
+set -e
+
+DIRS=(next node parcel rollup vite webpack4 webpack5 "$@")
+for i in "${!DIRS[@]}";
+do
+  DIRS[i]="$PWD/${DIRS[i]}"
+done
+
+while IFS='' read -r line; do
+  echo "Installing dependencies for $line"
+  (cd "$line" \
+   && tmp=$(mktemp) \
+   && jq --arg version "$version" '.dependencies."warp-contracts"=$version' package.json > "$tmp" \
+   && mv "$tmp" package.json)
+done < <(
+  find "${DIRS[@]}" -iname 'package.json' ! -path '*/node_modules/*' ! -path '*/.next/*' -exec dirname {} \;
+)
