32296046acb79eb0ad18aa2baf5522d0acccb8ba
asiaziola <ziola.jm@gmail.com>
2022-08-29 16:13:06 +0200

    cypress test v18

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'dtz6tr',
   e2e: {
     supportFile: false,
     video: false,
     screenshotOnRunFailure: false,
-    specPattern: '../../cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: '**/**/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
