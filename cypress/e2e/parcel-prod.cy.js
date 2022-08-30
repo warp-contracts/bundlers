fbdfbe626b3c40b2afe047ec3ae5a3809f1b1316
asiaziola <ziola.jm@gmail.com>
2022-08-30 11:25:22 +0200

    cypress test v38

---
+++ cypress/e2e/parcel-prod.cy.js
@@ -0,0 +1,5 @@
+describe('The Next JS Prod', () => {
+    it('successfully loads', () => {
+      cy.visit('http://localhost:1334')
+    });
+  });
---
+++ cypress/e2e/webpack4-prod.cy.js
@@ -0,0 +1,6 @@
+describe('The Next JS Prod', () => {
+    it('successfully loads', () => {
+      cy.visit('http://localhost:8090');
+    });
+  });
+  
